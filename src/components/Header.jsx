b493918efff51471e4d01e3de8bca124d0f67d54
Gregory Gerard <gregory@flatchr.io>
2023-11-17 13:25:08 +0100

    style: inline style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{color: 'red', textAlign: 'right'}}>A community of artists and art-lovers.</p>
     </header>
   );
 }
